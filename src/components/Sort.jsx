0dc8a4dd5029f54fbf5d820b85e56acc33166aaa
Alexandr Sekerin <blazzed21@gmail.com>
2024-03-11 13:15:43 +0300

    feat: make a popup for sorting list

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 import arrow from '../assets/arrow.svg';
 
 const Sort = () => {
+  const [visibleSelect, setVisibleSelect] = useState(false);
+  const [sortBy, setSortBy] = useState(0);
+  const sortList = ['popularity', 'price', 'alphabet'];
+
+  const onClickSort = (index) => {
+    setSortBy(index);
+    setVisibleSelect(!visibleSelect);
+  };
+
   return (
     <div className="sort">
-    <div className="sort__label">
-      <img src={arrow} alt="arrow" className='arrow' />
-      <b>Sort by:</b>
-      <span>popularity</span>
+      <div className="sort__label">
+        <img
+          src={arrow}
+          alt="arrow"
+          className={visibleSelect ? 'arrow rotate' : 'arrow '}
+        />
+        <b>Sort by:</b>
+        <span onClick={() => setVisibleSelect(!visibleSelect)}>
+          {sortList[sortBy]}
+        </span>
+      </div>
+      {visibleSelect && (<div className="sort__popup">
+        <ul>
+          {
+            sortList.map((sort, index) => (
+              <li
+                key={index}
+                className={sortBy === index ? 'active' : ''}
+                onClick={() => {
+                  onClickSort(index);
+                }}
+              >
+                {sort}
+              </li>
+            ))
+          }
+        </ul>
+      </div>)}
     </div>
-    <div className="sort__popup">
-      <ul>
-        <li className="active">popularity</li>
-        <li>price</li>
-        <li>alphabet</li>
-      </ul>
-    </div>
-  </div>
   );
-}
- 
+};
+
 export default Sort;
